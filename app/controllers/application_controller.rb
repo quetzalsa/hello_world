e145ae6dda2f68498bc9640a3e071995e408a090
queztalsa <quetzalsaez@gmail.com>
2017-08-10 02:30:17 -0300

    godbye

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world!"
+    render html: "¡Hola, mundo!"
+  end
+  def godbye
+    render html: "Godbye mens"
   end
 end
